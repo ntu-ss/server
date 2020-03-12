d2373f7fae0f2946e3c2b688f2a779beb0129de8
Petar Peychev <petarpeychev98@gmail.com>
2020-03-12 04:06:58 +0000

    Added initial netbeans ant project

---
+++ src/server/Server.java
@@ -0,0 +1,15 @@
+package server;
+
+/**
+ * Collects weather station data and updates the user clients.
+ */
+public class Server {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
